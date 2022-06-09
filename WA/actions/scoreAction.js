c7e7f1bba214680f8af04108a8030d6c7fc5b8fe
BUYUK Cem <cembuyuk7@gmail.com>
2022-06-09 15:11:43 +0200

    add blackJackGame

--- WA/actions/scoreAction.js
+++ WA/actions/scoreAction.js
@@ -1,9 +1,9 @@
-const url = "http://"+location.hostname+"/score"
+const url = "http://" + location.hostname + "/score"
 
 window.actions = {};
 
-window.actions.register = (userId,serverId,userName) =>
-    fetch(url+"/"+userId+"/register", {
+window.actions.register = (userId, serverId, userName) => {
+    fetch(url + "/" + userId + "/register", {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
@@ -13,3 +13,30 @@ window.actions.register = (userId,serverId,userName) =>
             serverId, userName
         })
     });
+}
+window.actions.getScore = (userId) => {
+    fetch(url + "/score/" + userId, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "GET",
+    }).then((res)=>{
+        return res
+    })
+}
+
+window.actions.addScore = (userId, score) => {
+    fetch(url + "/score/" + userId, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({
+            'score': score,
+        })
+    }).then((res)=>{
+        return res
+    })
+}
